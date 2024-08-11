179563ba7f42e652fae5e9c6227afc75f9b0884d
Ezra Tock <etock@u.rochester.edu>
2024-08-11 16:04:25 -0700

    add templater user script

---
+++ renderTasksTemplater.js
@@ -0,0 +1,26 @@
+//If editing this file, use
+module.exports = async (tp) => {
+    // Prompt for inputs
+    console.log("ok but it wokred tho");
+    const notePath = await tp.system.prompt("Enter the note path:", tp.file.path(true));
+    const renderLen = await tp.system.prompt("Enter the render length:", "1");
+    const renderIndex = await tp.system.prompt("Enter the render index:", "1");
+    const indent = await tp.system.prompt("Enter the indent level:", "0");
+
+    // Define the DataviewJS query template
+    const query = `
+\`\`\`dataviewjs
+let NOTE_PATH = "${notePath}";
+let RENDER_LEN = ${renderLen};
+let RENDER_INDEX = ${renderIndex};
+let INDENT = ${indent};
+
+console.log("success! " + notePath + renderLen + renderIndex + indent);
+\`\`\`
+`;
+
+    // Insert the query into the current note
+    const currentContent = await tp.file.content;
+    const updatedContent = currentContent + '\n' + query;
+    await tp.file.write(updatedContent);
+};
